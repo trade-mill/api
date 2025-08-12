e6dcfb5983a78c43efadd0808a3fb135ea5a87a8
k-kbk <kkbk0077@gmail.com>
2025-08-12 10:52:31 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/trademill/apiserver/TrademillApiApplication.java
@@ -0,0 +1,13 @@
+package trademill.apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrademillApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrademillApiApplication.class, args);
+	}
+
+}
