f93cf322a934b971a0149e35a851e2291b849724
Bokyeom <79684339+k-kbk@users.noreply.github.com>
2025-08-12 11:06:10 +0900

    chore: 프로젝트 초기화 (#1)

---
+++ src/test/java/trademill/apiserver/TrademillApiApplicationTests.java
@@ -0,0 +1,13 @@
+package trademill.apiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrademillApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
